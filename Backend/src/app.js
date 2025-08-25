3a7dd39979c2089dd09fc3d8a8ca126d2b020d75
Rexanimation <nickleister402@gmail.com>
2025-08-25 18:51:27 +0530

    badal dla

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -61,9 +61,7 @@ app.use('/api/chat', chatRoutes);
 
 
 app.get("*name", (req, res) => {
-
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-
+ res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 
